6e4df0ee837959fd20558781ac9f775e87ad348e
Gheorghe Aurel Pacurar <gheorghe.pacurar@gmail.com>
2025-01-25 17:20:31 +0200

    Code with comments

--- .github/myGeneratedCode/CodingComments.py
+++ .github/myGeneratedCode/CodingComments.py
@@ -10,6 +10,11 @@ class Car:
     def __init__(self, name, color, max_speed):
         """
         Initializes the Car object with the given name, color, and maximum speed.
+
+        Args:
+            name (str): The name of the car.
+            color (str): The color of the car.
+            max_speed (int): The maximum speed of the car in km/h.
         """
         self.name = name
         self.color = color
@@ -18,16 +23,37 @@ class Car:
     def __str__(self):
         """
         Returns a string representation of the Car object.
+
+        Returns:
+            str: A string describing the car.
         """
         return f"Car(Name: {self.name}, Color: {self.color}, MaxSpeed: {self.max_speed} km/h)"
 
     def get_name(self):
+        """
+        Returns the name of the car.
+
+        Returns:
+            str: The name of the car.
+        """
         return self.name
 
     def get_color(self):
+        """
+        Returns the color of the car.
+
+        Returns:
+            str: The color of the car.
+        """
         return self.color
 
     def get_max_speed(self):
+        """
+        Returns the maximum speed of the car.
+
+        Returns:
+            int: The maximum speed of the car in km/h.
+        """
         return self.max_speed
 
 if __name__ == "__main__":
